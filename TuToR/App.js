15cc67437bc28bff87bad6334040d907e874977b
Lethabo Simon Letsoalo <simonletsoalo70@gmail.com>
2026-02-13 22:43:47 +0200

    chore: update dependencies for build configurations

--- TuToR/App.js
+++ TuToR/App.js
@@ -1,7 +1,6 @@
-
-import {Provider} from "react-redux";
-import Store from './context/store'
-import {NavigationContainer} from "@react-navigation/native";
+import { Provider } from "react-redux";
+import Store from "./Context/store";
+import { NavigationContainer } from "@react-navigation/native";
 import LoadingScreen from "./Authentication/LoadingScreen";
 import WelcomeScreen from "./Authentication/WelcomeScreen";
 import AuthTypeScreen from "./Authentication/AuthTypeScreen";
@@ -19,71 +18,94 @@ import StudentDetails2 from "./Authentication/StudentDetails2";
 import TermsAndConditions from "./Authentication/TermsAndConditions";
 import NotificationScreen from "./Notifications/Notifications";
 import ProfileScreen from "./Profile/Profile";
-import PersonalInfo from './Profile/PersonalInfo';
+import PersonalInfo from "./Profile/PersonalInfo";
 import Payment from "./Profile/Payment";
-import Settings from './Profile/Settings';
-import About from './Profile/About';
-import ChangePassword from './Profile/ChangePassword';
-import NotificationsPreferences from './Profile/NotificationsPreferences';
-import PrivacySettings from './Profile/PrivacySettings';
-import MainApp from './MainComponents/MainApp'
-import TutorMainApp from './MainComponents/TutorMain'
+import Settings from "./Profile/Settings";
+import About from "./Profile/About";
+import ChangePassword from "./Profile/ChangePassword";
+import NotificationsPreferences from "./Profile/NotificationsPreferences";
+import PrivacySettings from "./Profile/PrivacySettings";
+import MainApp from "./MainComponents/MainApp";
+import TutorMainApp from "./MainComponents/TutorMain";
 import React from "react";
-import {createStackNavigator} from "@react-navigation/stack";
-import {StatusBar} from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "react-native";
 import TutorSearchScreen from "./Chat/TutorSearchScreen";
 import ChatScreen from "./Chat/ChatScreen";
 import PaymentsPage from "./Profile/PaymentsPage";
 import TransactionHistory from "./Profile/TransactionHistory";
-import TutorDetailsScreen from './FindTutor/TutorDetailsScreen';
+import TutorDetailsScreen from "./FindTutor/TutorDetailsScreen";
 import SessionsScreen from "./Sessions/Session";
-
 
 const Stack = createStackNavigator();
 
 export default () => {
   return (
-
-        <NavigationContainer>
-          <Provider store={Store}>
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
-            <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
-            <Stack.Screen name="AuthTypeScreen" component={AuthTypeScreen} />
-            <Stack.Screen name="SignInScreen" component={SignInScreen} />
-            <Stack.Screen name="ResetPasswordScreen" component={ResetPasswordScreen} />
-            <Stack.Screen name="EnterResetCodeScreen" component={EnterResetCodeScreen} />
-            <Stack.Screen name="TutorSearchScreen" component={TutorSearchScreen} />
-            <Stack.Screen name="TutorDetails1" component={TutorDetails1} />
-            <Stack.Screen name="TutorDetails2" component={TutorDetails2} />
-            <Stack.Screen name="TutorDetails3" component={TutorDetails3} />
-            <Stack.Screen name="TutorDetails4" component={TutorDetails4} />
-            <Stack.Screen name="TutorDetails5" component={TutorDetails5} />
-            <Stack.Screen name="ApplicationStatus" component={ApplicationStatus} />
-            <Stack.Screen name="StudentDetails1" component={StudentDetails1} />
-            <Stack.Screen name="StudentDetails2" component={StudentDetails2} />
-            <Stack.Screen name="TermsAndConditions" component={TermsAndConditions} />
-            <Stack.Screen name="NotificationScreen" component={NotificationScreen} />
-            <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
-            <Stack.Screen name="PersonalInfo" component={PersonalInfo} /> 
-            <Stack.Screen name="Payment" component={Payment} />
-            <Stack.Screen name="Settings" component={Settings} />
-            <Stack.Screen name="About" component={About} />
-            <Stack.Screen name="ChangePassword" component={ChangePassword} />
-            <Stack.Screen name="NotificationsPreferences" component={NotificationsPreferences} />
-            <Stack.Screen name="PrivacySettings" component={PrivacySettings} />
-            <Stack.Screen name="PaymentPage" component={PaymentsPage} />
-            <Stack.Screen name="TransactionHistory" component={TransactionHistory} />
-            <Stack.Screen name="ChatScreen" component={ChatScreen} />
-            <Stack.Screen name="MainApp" component={MainApp} />
-            <Stack.Screen name="TutorMainApp" component={TutorMainApp} />
-            <Stack.Screen name="TutorDetailsScreen" component={TutorDetailsScreen} />
-            <Stack.Screen name="SessionsScreen" component={SessionsScreen} />
-          </Stack.Navigator>
-          <StatusBar style="auto" />
-          </Provider>
-        </NavigationContainer>
-
-
+    <NavigationContainer>
+      <Provider store={Store}>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
+          <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+          <Stack.Screen name="AuthTypeScreen" component={AuthTypeScreen} />
+          <Stack.Screen name="SignInScreen" component={SignInScreen} />
+          <Stack.Screen
+            name="ResetPasswordScreen"
+            component={ResetPasswordScreen}
+          />
+          <Stack.Screen
+            name="EnterResetCodeScreen"
+            component={EnterResetCodeScreen}
+          />
+          <Stack.Screen
+            name="TutorSearchScreen"
+            component={TutorSearchScreen}
+          />
+          <Stack.Screen name="TutorDetails1" component={TutorDetails1} />
+          <Stack.Screen name="TutorDetails2" component={TutorDetails2} />
+          <Stack.Screen name="TutorDetails3" component={TutorDetails3} />
+          <Stack.Screen name="TutorDetails4" component={TutorDetails4} />
+          <Stack.Screen name="TutorDetails5" component={TutorDetails5} />
+          <Stack.Screen
+            name="ApplicationStatus"
+            component={ApplicationStatus}
+          />
+          <Stack.Screen name="StudentDetails1" component={StudentDetails1} />
+          <Stack.Screen name="StudentDetails2" component={StudentDetails2} />
+          <Stack.Screen
+            name="TermsAndConditions"
+            component={TermsAndConditions}
+          />
+          <Stack.Screen
+            name="NotificationScreen"
+            component={NotificationScreen}
+          />
+          <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+          <Stack.Screen name="PersonalInfo" component={PersonalInfo} />
+          <Stack.Screen name="Payment" component={Payment} />
+          <Stack.Screen name="Settings" component={Settings} />
+          <Stack.Screen name="About" component={About} />
+          <Stack.Screen name="ChangePassword" component={ChangePassword} />
+          <Stack.Screen
+            name="NotificationsPreferences"
+            component={NotificationsPreferences}
+          />
+          <Stack.Screen name="PrivacySettings" component={PrivacySettings} />
+          <Stack.Screen name="PaymentPage" component={PaymentsPage} />
+          <Stack.Screen
+            name="TransactionHistory"
+            component={TransactionHistory}
+          />
+          <Stack.Screen name="ChatScreen" component={ChatScreen} />
+          <Stack.Screen name="MainApp" component={MainApp} />
+          <Stack.Screen name="TutorMainApp" component={TutorMainApp} />
+          <Stack.Screen
+            name="TutorDetailsScreen"
+            component={TutorDetailsScreen}
+          />
+          <Stack.Screen name="SessionsScreen" component={SessionsScreen} />
+        </Stack.Navigator>
+        <StatusBar style="auto" />
+      </Provider>
+    </NavigationContainer>
   );
 };
--- TuToR/Authentication/LoadingScreen.js
+++ TuToR/Authentication/LoadingScreen.js
@@ -1,82 +1,86 @@
-import {ActivityIndicator, Alert, Image, View} from "react-native";
-import Logo from "../assets/Logo.png"
-import {useLayoutEffect} from "react";
-import {firebaseAuth, firestoreDB} from "../Config/firebaseConfig";
-import {doc, getDoc} from "firebase/firestore";
-import {setUser} from "../context/actions/userActions";
-import {useDispatch} from "react-redux";
-import {CommonActions} from "@react-navigation/native";
+import { ActivityIndicator, Alert, Image, View } from "react-native";
+import Logo from "../assets/Logo.png";
+import { useLayoutEffect } from "react";
+import { firebaseAuth, firestoreDB } from "../Config/firebaseConfig";
+import { doc, getDoc } from "firebase/firestore";
+import { setUser } from "../Context/actions/userActions";
+import { useDispatch } from "react-redux";
+import { CommonActions } from "@react-navigation/native";
 
-const LoadingScreen = ({navigation}) => {
-    const dispatch = useDispatch();
+const LoadingScreen = ({ navigation }) => {
+  const dispatch = useDispatch();
 
-     useLayoutEffect(() => {
-        checkLoggedUser();
-    }, []);
+  useLayoutEffect(() => {
+    checkLoggedUser();
+  }, []);
 
-    const checkLoggedUser = async () => {
-        firebaseAuth.onAuthStateChanged(async (user) => {
-            if (user?.uid) {
-                const docSnap = await getDoc(doc(firestoreDB, 'users', user?.uid));
-                if (docSnap.exists()) {
-                    const userData = docSnap.data();
-                    dispatch(setUser(userData));
+  const checkLoggedUser = async () => {
+    firebaseAuth.onAuthStateChanged(async (user) => {
+      if (user?.uid) {
+        const docSnap = await getDoc(doc(firestoreDB, "users", user?.uid));
+        if (docSnap.exists()) {
+          const userData = docSnap.data();
+          dispatch(setUser(userData));
 
-                    // Check if terms and conditions have been accepted
-                    if (userData.acceptedTerms) {
-                        // If terms are accepted, then check if the email is verified
-                        if (!user.emailVerified) {
-                            Alert.alert(
-                                'Email Verification Required',
-                                'Please verify your email to proceed. A verification email has been sent.',
-                                [
-                                    {
-                                        text: 'OK',
-                                        onPress: async () => {
-                                            await firebaseAuth.signOut();
-                                          }
-                                    }
-                                ]
-                            );
-                            return; // Prevent further execution if email is not verified
-                        }
-
-                        // Proceed with navigation if email is verified
-                        if (userData.role === 'Student') {
-                            navigation.dispatch(
-                                CommonActions.reset({
-                                    index: 0,
-                                    routes: [{ name: 'MainApp' }],
-                                })
-                            );
-                        } else if (userData.role === 'Tutor') {
-                            navigation.dispatch(
-                                CommonActions.reset({
-                                    index: 0,
-                                    routes: [{ name: 'TutorMainApp' }],
-                                })
-                            );
-                        }
-                    } else {
-                        // If terms and conditions are not accepted, navigate to Terms and Conditions screen
-                        navigation.navigate('TermsAndConditions', { userId: user.uid });
-                    }
-                }
-            } else {
-                // If no user is logged in, redirect to SignInScreen after 2 seconds
-                setTimeout(() => {
-                    navigation.replace("SignInScreen");
-                }, 4000);
+          // Check if terms and conditions have been accepted
+          if (userData.acceptedTerms) {
+            // If terms are accepted, then check if the email is verified
+            if (!user.emailVerified) {
+              Alert.alert(
+                "Email Verification Required",
+                "Please verify your email to proceed. A verification email has been sent.",
+                [
+                  {
+                    text: "OK",
+                    onPress: async () => {
+                      await firebaseAuth.signOut();
+                    },
+                  },
+                ],
+              );
+              return; // Prevent further execution if email is not verified
             }
-        });
-    };
 
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Image source={Logo} style={{ width: 150, height: 150, marginBottom:20 , borderRadius:100 }} resizeMode="contain" />
-            <ActivityIndicator size={"large"} color="#00243a" />
-        </View>
-    );
+            // Proceed with navigation if email is verified
+            if (userData.role === "Student") {
+              navigation.dispatch(
+                CommonActions.reset({
+                  index: 0,
+                  routes: [{ name: "MainApp" }],
+                }),
+              );
+            } else if (userData.role === "Tutor") {
+              navigation.dispatch(
+                CommonActions.reset({
+                  index: 0,
+                  routes: [{ name: "TutorMainApp" }],
+                }),
+              );
+            }
+          } else {
+            // If terms and conditions are not accepted, navigate to Terms and Conditions screen
+            navigation.navigate("TermsAndConditions", { userId: user.uid });
+          }
+        }
+      } else {
+        // If no user is logged in, redirect to SignInScreen after 2 seconds
+        setTimeout(() => {
+          navigation.replace("SignInScreen");
+        }, 4000);
+      }
+    });
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Image
+        source={Logo}
+        style={{ width: 150, height: 150, marginBottom: 20, borderRadius: 100 }}
+        resizeMode="contain"
+      />
+      <ActivityIndicator size={"large"} color="#00243a" />
+    </View>
+  );
 };
 
 export default LoadingScreen;
